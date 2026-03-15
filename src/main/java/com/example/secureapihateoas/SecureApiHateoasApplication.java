a4f3a3cbbcbcc04fdf416cf4053e97d6bba0b9b6
Estcepoire <rajaoharisaona@gmail.com>
2026-03-15 20:20:04 +0300

    init project

---
+++ src/main/java/com/example/secureapihateoas/SecureApiHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.example.secureapihateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureApiHateoasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureApiHateoasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/secureapihateoas/SecureApiHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secureapihateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureApiHateoasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
